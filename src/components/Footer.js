2cb863e6f0f96512b83805c5438f5f34b0da2478
Victor Peinado <vpeinado263@gmail.com>
2025-11-29 16:33:26 -0300

    Personalizar Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,27 @@ import { ThemeContext } from "@/context/ThemeContextProvider";
 import { useContext } from "react";
 
 const Footer = () => {
-  const data = useContext(ThemeContext);
-  const [toggleTheme] = data;
+  const { theme } = useContext(ThemeContext);
 
   return (
-    <div
-      style={{
-        height: "100vh",
-      }}
-      className={toggleTheme}
-    >
-      Footer
-    </div>
+    <footer className={theme}>
+      <div className="footer-content">
+        <p>© 2025 - Mi Proyecto React/Next.js Sistema Claro - Oscuro</p>
+      </div>
+
+      <style jsx>{`
+        footer {
+          padding: 2rem;
+          text-align: center;
+          border-top: 1px solid var(--border);
+          margin-top: 3rem;
+        }
+
+        .footer-content p {
+          margin: 5px 0;
+        }
+      `}</style>
+    </footer>
   );
 };
 
